f29d4a397c90b26a51bb04dfbaa96fdfc59f8c3b
Andrew <andrew.ramell@gmail.com>
2017-12-21 17:20:32 -0500

    added controller code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+
+  end
+
+  def new
+    @task = Task.new
+
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      respond_to do |f|
+        f.html {render :show}
+        f.json {render json: @task}
+      end
+  end
+
+  def show
+    @task = Task.find_by(:id)
+  end
+  
+  def edit
+    @task = Task.find_by(:id)
+
+  end
+
+  def update
+
+  end 
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
+
+  end
 end
